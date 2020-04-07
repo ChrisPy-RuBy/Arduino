9fa1b00aa0f8943f6796da27134a276233d12d85
chris woodall <christopherhwoodall@gmail.com>
2020-04-07 21:23:27 +0100

    x

--- Servo/Servo.ino
+++ Servo/Servo.ino
@@ -15,17 +15,16 @@ void setup()
 void loop()
 {
 		// scan from 0 to 180 degrees
-		for(angle = 0; angle < 180; angle++)
+		for(angle = 0; angle < 360; angle++)
 		{
 				servo.write(angle);
 				delay(15);
 		}
 		// now scan back from 180 to 0 degrees
-		for(angle = 180; angle > 0; angle--)
+		for(angle = 360; angle > 0; angle--)
 		{
 				servo.write(angle);
 				delay(15);
 		}
-}
 }
 
