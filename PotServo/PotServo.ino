c5b0a09d86f6d49f969029c6c8f06028223788ef
Chris Woodall <christopherhwoodall@gmail.com>
2020-04-07 21:24:47 +0100

    x

--- PotServo/PotServo.ino
+++ PotServo/PotServo.ino
@@ -1,6 +1,17 @@
+nclude <Servo.h>
 
-void setup() {
+int potPin = 0;
+int servoPin = 9;
+Servo servo;
+
+void setup()
+{
+        servo.attach(servoPin);
 }
 
-void loop() {
+void loop()
+{
+        int reading = analogRead(potPin);     // 0 to 1023
+        int angle = reading / 6;              // 0 to 180-ish
+        servo.write(angle);
 }
