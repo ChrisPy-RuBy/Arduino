c2c2cc9d91ab68a5b544681fe517ecd6559edd04
Chris Woodall <christopherhwoodall@gmail.com>
2020-06-13 14:51:01 +0100

    z

--- pythonforarduino/basicstreamer.py
+++ pythonforarduino/basicstreamer.py
@@ -5,4 +5,5 @@ import datetime
 ser = serial.Serial('/dev/ttyACM0', 9600)
 
 while True:
-    print(datetime.datetime.now().isoformat(), ser.readline())
+    row = ser.readline()
+    print(datetime.datetime.now().isoformat(), row)
