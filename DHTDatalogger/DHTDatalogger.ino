5d30323d8b732e239a88e5bc110ab482fb6d1165
chris woodall <christopherhwoodall@gmail.com>
2020-06-19 22:06:54 +0100

    added debug led

--- DHTDatalogger/DHTDatalogger.ino
+++ DHTDatalogger/DHTDatalogger.ino
@@ -7,17 +7,24 @@
 Physical setup (What pins connect to what?)
 ---> SENSOR
 DHTPIN = 2
+
 ---> SD CARD
 MISO = 13
 MOSI = 12
 SCK = 11
 CS = 10
+
 ---> RTC
 SCL -> A5
 SDA -> A4
 
 ---> Ext Thermo
 DS -> 4
+
+--->  Debug LED
+RED -> 5
+BLUE -> 6
+GREEn -> 7
 
 LIBRARIES REQUIRED
 -> DHT
@@ -28,6 +35,8 @@ LIBRARIES REQUIRED
 -> OneWire
 -> dallasTemperature
 */
+
+
 
 // setup DHT
 #include "DHT.h"
@@ -53,7 +62,17 @@ RTC_DS1307 rtc;
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
 
+const int RED = 5;
+const int GREEN = 6;
+const int BLUE = 7;
+
 void setup() {
+
+	pinMode(RED, OUTPUT);
+	pinMode(BLUE, OUTPUT);
+	pinMode(GREEN, OUTPUT);
+
+	digitalWrite(RED, HIGH);
 	Serial.begin(9600);
 	Serial.println("Initializing SD card...");
 	if (!SD.begin(10)) {
@@ -82,7 +101,11 @@ void loop() {
 
     // Reading the sensor	
 	delay(2000);
-	Serial.println("In loop");
+	
+	digitalWrite(RED, LOW);
+	digitalWrite(GREEN, HIGH);
+	delay(100);
+	digitalWrite(GREEN, LOW);
 	sensors.requestTemperatures();
 
 	float externalTemp = sensors.getTempCByIndex(0);
@@ -90,6 +113,12 @@ void loop() {
 	String time = now.timestamp();
     float h = dht.readHumidity();
     float temp = dht.readTemperature();
+
+	if (isnan(now.unixtime()) || isnan(externalTemp) || isnan(h) || isnan(temp)) {
+		digitalWrite(BLUE, HIGH);
+		digitalWrite(GREEN, LOW);
+	}
+
 	serialWrite(time, externalTemp, h, temp);
 	saveData(time, externalTemp, h, temp);
 }
@@ -111,10 +140,10 @@ void serialWrite(String time, float externalTemp, float dhth, float dhttemp) {
   // Function for debug of humidity sensor
 
   // Check if any reads failed and exit early (to try again).
-  if (isnan(dhth) || isnan(dhttemp)) {
-    Serial.println(F("Failed to read from DHT sensor!"));
-    return;
-  }
+  //if (isnan(dhth) || isnan(dhttemp)) {
+  //  Serial.println(F("Failed to read from DHT sensor!"));
+  // return;
+  //}
   Serial.print(F("Time: "));
   Serial.print(time);
   Serial.print(F(" Ext Temp: "));
